289d68d309a6afabc5119600fdb5942509ca9fe8
yuan chang <yuanchang@dhcp-nyc-317.eduroam.cornell.edu>
2018-05-02 17:00:18 -0400

    before deploy to heroku

---
+++ test.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os.path
+import re
+import sys
+import tarfile
+import json
+from tutorials.image.imagenet import classify_image as cls
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
+from flask import Flask
+from flask import jsonify
+from flask import Response
+from flask import request
+import urllib.request
+
+app = Flask(__name__)
+
+@app.route("/objDetection")
+def hello():
+#example data:
+    url = request.args.get('url', default = "", type = str)
+    urllib.request.urlretrieve(url, "image.jpg")
+    js = cls.main("image.jpg")
+#then do this
+    return Response(json.dumps(js),  mimetype='application/json')
+   
+
